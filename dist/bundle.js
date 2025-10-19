7cd78b6d3cc532d6c4aeee536e032f1927a664cd
FrBosquet@MBA <21219312+FrBosquet@users.noreply.github.com>
2025-10-19 09:19:12 +0200

    Initial commit

---
+++ dist/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = 'Hello webpack!';
+  return element;
+}
+
+document.body.appendChild(component());
+
+/******/ })()
+;
