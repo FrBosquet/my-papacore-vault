9a1f77ea192a4b554a8af9224e217ccc0133a810
FrBosquet@MBA <21219312+FrBosquet@users.noreply.github.com>
2025-11-01 16:12:45 +0100

    Improve tooling

--- src/icons.d.ts
+++ src/icons.d.ts
@@ -1,6 +1,15 @@
-export type IconName =
-  | "folder-kanban"
-  | "gamepad"
-  | "gamepad-2"
-  | "headphones"
+const Icons = [
+  'folder-kanban',
+  'gamepad',
+  'gamepad-2',
+  'headphones',
+  'disc',
+  'disc-2',
+  'disc-3',
+  'star',
+  'user',
+  'user-round'
+]
+
+export type IconName = typeof Icons[number]
 
