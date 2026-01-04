06a8c9639a5fce61f2bc9f2fc06de3d363b17d28
FrBosquet@MBA <21219312+FrBosquet@users.noreply.github.com>
2026-01-04 11:09:31 +0100

    Add habits streak helper

---
+++ src/Datacore/utils/habits.test.ts
@@ -0,0 +1,79 @@
+import { MarkdownPage } from '@blacksmithgu/datacore'
+import { DateTime } from 'luxon'
+import { calculateStreak } from './habits'
+
+describe('habits utils', () => {
+  describe('calculateStreak', () => {
+    const notes = [
+      {
+        $path: 'Journal/2026-01-01.md',
+      },
+      {
+        $path: 'Journal/2026-01-03.md',
+      },
+      {
+        $path: 'Journal/2026-01-02.md',
+      },
+    ] as MarkdownPage[]
+    
+    it('should exist', () => {
+      expect(calculateStreak).toBeDefined()
+    })
+
+    it('should calculate a streak of 3 days', () => {
+      const referenceDate = dc.coerce.date('2026-01-03') as DateTime;
+
+      const streak = calculateStreak(notes, referenceDate)
+      expect(streak).toHaveProperty('type', 'streak')
+      expect(streak).toHaveProperty('days', 3)
+    })
+
+    it('should calculate a streak of 2 day', () => {
+      const referenceDate = dc.coerce.date('2026-01-02') as DateTime;
+
+      const streak = calculateStreak(notes, referenceDate)
+      expect(streak).toHaveProperty('type', 'streak')
+      expect(streak).toHaveProperty('days', 2)
+    })
+
+    it('should calculate a streak of 1 day', () => {
+      const referenceDate = dc.coerce.date('2026-01-01') as DateTime;
+
+      const streak = calculateStreak(notes, referenceDate)
+      expect(streak).toHaveProperty('type', 'streak')
+      expect(streak).toHaveProperty('days', 1)
+    })
+
+    it('should calculate an hiatus', () => {
+      const referenceDate = dc.coerce.date('2026-01-07') as DateTime;
+
+      const streak = calculateStreak(notes, referenceDate)
+      expect(streak).toHaveProperty('type', 'hiatus')
+      expect(streak).toHaveProperty('days', 4)
+    })
+
+    it('should calculate a never streak', () => {
+      const referenceDate = dc.coerce.date('2026-01-05') as DateTime;
+
+      const streak = calculateStreak([], referenceDate)
+      expect(streak).toHaveProperty('type', 'never')
+      expect(streak).toHaveProperty('days', 0)
+    })
+
+    it('should calculate a streak of one with only one note', () => {
+      const referenceDate = dc.coerce.date('2026-01-01') as DateTime;
+
+      const streak = calculateStreak([notes[0]], referenceDate)
+      expect(streak).toHaveProperty('type', 'streak')
+      expect(streak).toHaveProperty('days', 1)
+    })
+
+    it('should calculate a streak of one with mixed list of notes', () => {
+      const referenceDate = dc.coerce.date('2026-01-03') as DateTime;
+
+      const streak = calculateStreak([notes[0], notes[1]], referenceDate)
+      expect(streak).toHaveProperty('type', 'streak')
+      expect(streak).toHaveProperty('days', 1)
+    })
+  })
+})
---
+++ src/Datacore/utils/habits.ts
@@ -0,0 +1,44 @@
+import type { MarkdownPage } from "@blacksmithgu/datacore";
+import { DateTime } from "luxon";
+import { getDailyNoteDatetime } from "./files";
+
+type Streak = {
+  days: number;
+  type: 'streak' | 'hiatus' | 'never'
+}
+
+export const calculateStreak = (notes: MarkdownPage[], referenceDate: DateTime): Streak => {
+  if(notes.length === 0) {
+    return { days: 0, type: 'never' }
+  }
+
+  const isoReferenceDate = referenceDate.toISODate() as string;
+  // clone the notes array to avoid mutating the original array
+  const dates = [...notes]
+    // sort by date descending
+    .sort((a, b) => b.$path.localeCompare(a.$path))
+    // filter out notes that are after the reference date
+    .filter(note => note.$path.replace('.md', '').replace('Journal/', '').localeCompare(isoReferenceDate) <= 0)
+    .map(note => getDailyNoteDatetime(note.$path));
+
+  const daysSinceFirstNote = referenceDate.diff(dates[0]).as('days');
+
+  if (referenceDate.diff(dates[0]).as('days') > 0) {
+    return {
+      days: daysSinceFirstNote,
+      type: 'hiatus',
+    }
+  }
+
+  if (dates.length === 1) { return { days: 1, type: 'streak' } }
+
+  for (var i = 1; i < dates.length; i++) {
+    const prev = dates[i - 1];
+    const current = dates[i];
+    const diff = prev.diff(current).as('days');
+
+    if (diff !== 1) { return { days: i, type: 'streak' } }
+  }
+
+  return { days: dates.length, type: 'streak' }
+}
