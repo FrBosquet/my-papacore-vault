1f8df984056424c8f86e958d66c4a3bc732f822a
FrBosquet@MBA <21219312+FrBosquet@users.noreply.github.com>
2026-01-09 18:40:52 +0100

    Complete the game hub

--- src/Datacore/components/games/get-game-modal.tsx
+++ src/Datacore/components/games/get-game-modal.tsx
@@ -7,18 +7,25 @@ import { Dialog, useDialog } from "../shared/dialog"
 
 export const GetGameDataModal = ({ apiKey, file }: { apiKey: string, file: MarkdownPage }) => {
   const { ref: dialogRef, close } = useDialog()
+  const [state, setState] = useState<'loading' | 'success' | 'error'>('loading')
   const fileName = file.$name
 
   const [result, setResult] = useState<GameInfo | null>(null)
 
   const handleOpen = async () => {
-    const result = await getGameData(fileName, apiKey)
+    try {
+      const result = await getGameData(fileName, apiKey)
 
-    setResult(result)
+      setResult(result)
+      setState('success')
+    } catch (error) {
+      setState('error')
+      alert(error instanceof Error ? error.message : error)
+    }
   }
 
   const handleApplyData = async () => {
-    if(!result) return
+    if (!result) return
 
     await setPageFrontmatterValue(file, 'year', result.year)
     await setPageFrontmatterValue(file, 'image', result.image)
@@ -27,6 +34,37 @@ export const GetGameDataModal = ({ apiKey, file }: { apiKey: string, file: Markd
     await setPageFrontmatterValue(file, 'hltb', result.hltb)
 
     close()
+  }
+
+  const renderContent = () => {
+    switch (state) {
+      case 'success':
+        if (!result) throw new Error('Result is null and state is success')
+
+        return <div className="flex flex-col gap-2">
+          <img src={result.image} alt={fileName} className="w-full h-auto" />
+          <p>Año: {result.year}</p>
+          <p>Precio: {result.price}</p>
+          <p>Puntuación: {result.metacritic}</p>
+          <p>Duración: {result.hltb}</p>
+          <Button onClick={handleApplyData}>Aplicar datos</Button>
+          <aside className="h-0.5 bg-primary-500 w-full my-2"></aside>
+
+          {result.metacriticDataUrl && <p className={'text-sm text-primary-500'}><a href={result.metacriticDataUrl}>Metacritic</a></p>}
+          {result.hltbDataUrl && <p className={'text-sm text-primary-500'}><a href={result.hltbDataUrl}>HowLongToBeat</a></p>}
+          {result.dataUrl && <p className={'text-sm text-primary-500'}><a href={result.dataUrl}>Informacion</a></p>}
+        </div>
+      case 'loading':
+        return <div className="flex flex-col gap-2 justify-center items-center">
+          <p>Cargando...</p>
+          <dc.Icon icon="loader" className="animate-spin [&>svg]:size-10" />
+        </div>
+      case 'error':
+        return <div className="flex flex-col gap-2">
+          <p>Error obteniendo los datos del juego</p>
+          <Button onClick={handleOpen} icon="refresh-ccw">Intentar de nuevo</Button>
+        </div>
+    }
   }
 
   return (
@@ -39,14 +77,7 @@ export const GetGameDataModal = ({ apiKey, file }: { apiKey: string, file: Markd
       }}
       onOpen={handleOpen}
     >
-      {result ? <div className="flex flex-col gap-2">
-        <img src={result.image} alt={fileName} className="w-full h-auto" />
-        <p>Año: {result.year}</p>
-        <p>Precio: {result.price}</p>
-        <p>Puntuación: {result.metacritic}</p>
-        <p>Duración: {result.hltb}</p>
-        <Button onClick={handleApplyData}>Aplicar datos</Button>
-      </div> : <div>Loading...</div>}
+      {renderContent()}
     </Dialog>
   )
 }
--- src/Datacore/utils/perplexity.ts
+++ src/Datacore/utils/perplexity.ts
@@ -31,10 +31,21 @@ export type GameInfo = {
   price: number
   metacritic: number
   hltb: number
+  metacriticDataUrl: string
+  hltbDataUrl: string
+  dataUrl: string
 }
 
 export const getGameData = async (fileName: string, apiKey: string): Promise<GameInfo> => {
-  const prompt = `Search only in steam.com, metacritic.com and howlongtobeat.com. Do not take any other source of information into account. Return a JSON with the data for the game ${fileName}. A valid JSON stringified with just the JSON, no other text. You should search in steam.com to get 'image' string for the game cover in the best resolution you can find, the 'year' integer for the year of release, the 'price' float for the game price in euros without discounts. You should go to metacritic.com to get the 'metacritic' float for the users game score from 0 to 100. You should go to howlongtobeat.com to get the 'hltb' float for the game duration. Do not visit other webs, thats the only source of information you need.
+  const prompt = `Return a JSON with the data for the game ${fileName}. A valid JSON stringified with just the JSON, no other text.
+  
+  You should search in steam.com for the 'year' integer for the year of release, the 'price' float for the game price in euros without discounts. If the game is not listed in steam, look for other sources of information to get the year and price. Provide a dataUrl string with a link with the page of the store where you found the information.
+  
+  You should get 'image' string for the game cover in the best resolution you can find. If its an steam game, try to get the image from steam.com domain. If its not a steam game, try to get the image from upload.wikimedia.com domain. If both fails, try to get the image from howlongtobeat.com domain. Don't made up the strings, actually search for references to the URI. If all fails, return null, never return an string from any other domain.
+
+  You should go to metacritic.com to get the 'metacritic' float for the users game score from 0 to 100. No other source is valid for the metacritic score. If you can find the score, just return null. Provide a metacriticDataUrl string with a link with the page of the store where you found the information.
+  
+  You should go to howlongtobeat.com to get the 'hltb' float for the game duration. Do not visit other webs, thats the only source of information you need for completion time. Provide a hltbDataUrl string with a link to the howlongtobat page where you found the information. Make sure the url points to the right game page, not any other game's page.
     `
 
   const result = await fetch('https://www.franbosquet.com/api/ask', {
@@ -58,9 +69,17 @@ export const getGameData = async (fileName: string, apiKey: string): Promise<Gam
   const jsonContent = response.data.choices[0].message.content
 
   try {
-    return JSON.parse(jsonContent)
+    const json = JSON.parse(jsonContent)
+
+    console.log(json)
+
+    if (json.error) {
+      throw new Error(json.error)
+    }
+
+    return json
   } catch (error) {
     console.error(error)
-    throw new Error('Error parsing the JSON response. Comprueba la consola para más detalles.')
+    throw new Error(error instanceof Error ? error.message : 'Error parsing the JSON response. Comprueba la consola para más detalles.')
   }
 }
