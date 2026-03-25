ab85877358bc625245bd4a4827f2bea0498ad814
FrBosquet@MBA <21219312+FrBosquet@users.noreply.github.com>
2026-03-25 17:09:51 +0100

    Remove a console log

--- src/Datacore/components/shared/scroller.tsx
+++ src/Datacore/components/shared/scroller.tsx
@@ -20,8 +20,6 @@ export const Scroller = ({ children, className, wrapperClassName }: Props) => {
       const scrollHeight = element.scrollHeight
       const clientHeight = element.clientHeight
 
-
-      console.log(scrollPosition, scrollHeight, clientHeight)
       // If content fits within container (no scrolling needed), hide both bands
       if (scrollHeight <= clientHeight + 5) {
         setScrollState('no-scroll')
